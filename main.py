1b6657053a17656bd47d35c1c79afa439a67ce4e
yaswanthkumarch <82691830+yaswanthkumarch@users.noreply.github.com>
2021-08-17 09:34:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+#TAKE TWO INPUTS AND STORE IN VARIABLE X AND Y ,FIND X TO THE POWER Y.
+x=int(input("enter a first number:"))
+y=int(input("enter a second number:"))
+res=x**y
+print('the power of two numbers is:',res)
